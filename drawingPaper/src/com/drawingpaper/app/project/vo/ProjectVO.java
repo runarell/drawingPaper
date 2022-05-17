87a7227b7cb1f63798376915e8a98c844c11bafd
lje1343 <lje1343@gmail.com>
2022-05-17 18:50:39 +0900

    VO, xml

---
+++ drawingPaper/src/com/drawingpaper/app/project/vo/ProjectVO.java
@@ -0,0 +1,144 @@
+package com.drawingpaper.app.project.vo;
+
+public class ProjectVO {
+	private int pro_no;
+	private int user_no;
+	private String pro_title;
+	private String pro_category;
+	private String pro_start;
+	private String pro_end;
+	private String pro_addr;
+	private String pro_addrdetail;
+	private String pro_goalprice;
+	private String pro_ticketprice;
+	private String pro_repimage;
+	private String pro_imges;
+	private String pro_content;
+	private boolean pro_ongoing;
+	private String pro_achieveprice;
+	
+	public ProjectVO() {;}
+
+	public int getPro_no() {
+		return pro_no;
+	}
+
+	public void setPro_no(int pro_no) {
+		this.pro_no = pro_no;
+	}
+
+	public int getUser_no() {
+		return user_no;
+	}
+
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+
+	public String getPro_title() {
+		return pro_title;
+	}
+
+	public void setPro_title(String pro_title) {
+		this.pro_title = pro_title;
+	}
+
+	public String getPro_category() {
+		return pro_category;
+	}
+
+	public void setPro_category(String pro_category) {
+		this.pro_category = pro_category;
+	}
+
+	public String getPro_start() {
+		return pro_start;
+	}
+
+	public void setPro_start(String pro_start) {
+		this.pro_start = pro_start;
+	}
+
+	public String getPro_end() {
+		return pro_end;
+	}
+
+	public void setPro_end(String pro_end) {
+		this.pro_end = pro_end;
+	}
+
+	public String getPro_addr() {
+		return pro_addr;
+	}
+
+	public void setPro_addr(String pro_addr) {
+		this.pro_addr = pro_addr;
+	}
+
+	public String getPro_addrdetail() {
+		return pro_addrdetail;
+	}
+
+	public void setPro_addrdetail(String pro_addrdetail) {
+		this.pro_addrdetail = pro_addrdetail;
+	}
+
+	public String getPro_goalprice() {
+		return pro_goalprice;
+	}
+
+	public void setPro_goalprice(String pro_goalprice) {
+		this.pro_goalprice = pro_goalprice;
+	}
+
+	public String getPro_ticketprice() {
+		return pro_ticketprice;
+	}
+
+	public void setPro_ticketprice(String pro_ticketprice) {
+		this.pro_ticketprice = pro_ticketprice;
+	}
+
+	public String getPro_repimage() {
+		return pro_repimage;
+	}
+
+	public void setPro_repimage(String pro_repimage) {
+		this.pro_repimage = pro_repimage;
+	}
+
+	public String getPro_imges() {
+		return pro_imges;
+	}
+
+	public void setPro_imges(String pro_imges) {
+		this.pro_imges = pro_imges;
+	}
+
+	public String getPro_content() {
+		return pro_content;
+	}
+
+	public void setPro_content(String pro_content) {
+		this.pro_content = pro_content;
+	}
+
+	public boolean isPro_ongoing() {
+		return pro_ongoing;
+	}
+
+	public void setPro_ongoing(boolean pro_ongoing) {
+		this.pro_ongoing = pro_ongoing;
+	}
+
+	public String getPro_achieveprice() {
+		return pro_achieveprice;
+	}
+
+	public void setPro_achieveprice(String pro_achieveprice) {
+		this.pro_achieveprice = pro_achieveprice;
+	}
+	
+	
+
+}
---
+++ drawingPaper/src/com/drawingpaper/app/supporter/vo/SupporterVO.java
@@ -0,0 +1,26 @@
+package com.drawingpaper.app.supporter.vo;
+
+public class SupporterVO {
+	private int user_no;
+	private int pro_no;
+	
+	public SupporterVO() {;}
+
+	int getUser_no() {
+		return user_no;
+	}
+
+	void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+
+	int getPro_no() {
+		return pro_no;
+	}
+
+	void setPro_no(int pro_no) {
+		this.pro_no = pro_no;
+	}
+	
+	
+}
