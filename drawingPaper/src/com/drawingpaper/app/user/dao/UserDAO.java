785803ff0facb40c591e45a5dac40b09fae30686
thirdK <dla0625@gmail.com>
2022-05-21 17:26:57 +0900

    no message

--- drawingPaper/src/com/drawingpaper/app/user/dao/UserDAO.java
+++ drawingPaper/src/com/drawingpaper/app/user/dao/UserDAO.java
@@ -51,4 +51,34 @@ public class UserDAO {
 	   }
 
 	   // 메인 화면 세션(유저 이름)띄워주기
+	   
+	   
+	// 일반 로그인
+		public boolean login(String user_email, String user_pw) {
+			HashMap<String, String> userMap = new HashMap<>();
+			userMap.put("user_email", user_email);
+			userMap.put("user_pw", user_pw);
+			return (Integer)(sqlSession.selectOne("User.login", userMap)) == 1;
+		}
+		
+		
+		//이메일(id) 검사 	-> true(중복)
+		public boolean checkEmail(String user_email) {
+			return (Integer)(sqlSession.selectOne("User.checkEmail", user_email)) == 1;
+		}
+		
+		//이메일로 회원가입
+		public void joinEmail(HashMap<String, String> emailJoinInfo) {
+			sqlSession.insert("User.joinEmail", emailJoinInfo);
+		}
+
+		//이메일로 유저번호 찾기
+		public int getUserNoByEmail(String user_email) {
+			return (Integer)sqlSession.selectOne("User.getUserNoByEmail", user_email);
+		}
+		
+		//카카오 회원가입
+		public void joinKakao(HashMap<String, String> kakaoJoinInfo) {
+			sqlSession.insert("User.joinKakao", kakaoJoinInfo);
+		}
 	}
